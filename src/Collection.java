1e01136e361568b7eece878f3b43bf906d75fbb3
Sakura <sakurathetrans@gmail.com>
2025-03-20 21:11:04 -0400

    Initial commit

---
+++ src/Collection.java
@@ -0,0 +1,21 @@
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class ContactCollection implements Iterable{
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public void forEach(Consumer action) {
+        Iterable.super.forEach(action);
+    }
+
+    @Override
+    public Spliterator spliterator() {
+        return Iterable.super.spliterator();
+    }
+}
